07cff3e93e4c8f3fc4dd3dafd6e8adaa73f1d77f
hyeonseo-chae <eric71804122@gmail.com>
2023-09-29 16:12:06 +0900

    test

--- mcp3008.py
+++ mcp3008.py
@@ -1,1 +1,29 @@
-/home/hyeonseo/mcp3008.py
+import spidev, time
+import RPi.GPIO as GPIO
+
+led = 18
+led2 = 23
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(led,GPIO.OUT)
+GPIO.setup(led2,GPIO.OUT)
+spi = spidev.SpiDev()
+spi.open(0,0)
+spi.max_speed_hz=1350000
+
+def analog_read(channel):
+    r = spi.xfer2([1,(8+channel) << 4,0])
+    adc_out = ((r[1]&3)<<8)+r[2]
+    return adc_out
+
+while True:
+    GPIO.output(led2,GPIO.HIGH)
+    reading = analog_read(1)
+    voltage = reading * 2.2/1024
+    print("Reading=%d\tVoltage=%f"%(reading,voltage))
+    time.sleep(1)
+    if reading<500:
+        GPIO.output(led,GPIO.HIGH)
+        print("LED ON")
+    else:
+        GPIO.output(led,GPIO.LOW)
+        print("LED OFF")
